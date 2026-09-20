ca995e044797bd1a2d89a1fb172a9545d0d3a89f
sethyadav <sethyadav169@gmail.com>
2026-09-20 14:27:08 +0530

    fix production cors

--- backend/server.js
+++ backend/server.js
@@ -29,12 +29,20 @@ connectDB();
 // MIDDLEWARE
 // ================================
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     credentials: true,
+//   })
+// );
+
+cors({
+  origin: [
+    "http://localhost:3000",
+    "https://pro-stack-hub-crafcart-15xc.vercel.app",
+  ],
+  credentials: true,
+})
 
 app.use(express.json());
 
